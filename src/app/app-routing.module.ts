b0af2276db3ae299d2e60ee0b7b1b6fd251f1abd
trungtrucnguyen <trungtruc1094@gmail.com>
2018-09-24 22:47:46 +0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule, Router } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: 'recipes', pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent, children: [
         { path: '', component: RecipeStartComponent },
-        { path: ':id', component: RecipeDetailComponent }
+        { path: 'new', component: RecipeEditComponent },
+        { path: ':id', component: RecipeDetailComponent },
+        { path: ':id/edit', component: RecipeEditComponent }
     ] },
     { path: 'shopping-list', component: ShoppingListComponent }
 ];
@@ -19,4 +22,4 @@ const appRoutes: Routes = [
     exports: [RouterModule]
 })
 
-export class AppRoutingModule{}
+export class AppRoutingModule {}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -18,7 +18,7 @@ export class RecipeService{
             new Ingredient('Meat', 2),
             new Ingredient('Salad', 5)
         ]),
-        new Recipe('A Test Recipe 2', 
+        new Recipe('A Test Recipe 2',
         'This is a simple text', 
         'https://img.taste.com.au/UCkD8VfP/w1200-h630-cfill/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg',
         [
@@ -27,11 +27,15 @@ export class RecipeService{
         ])
     ];
 
-    getRecipe(){
+    getRecipe() {
         return this.recipes.slice();
     }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]){
+    getRecipeById(id: number) {
+        return this.recipes[id];
+    }
+    
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.shoppingListService.addIngredientsToList(ingredients);
     }
 }
